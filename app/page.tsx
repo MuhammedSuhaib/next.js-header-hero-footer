a25df914c27ea69ac7b026c63baf24a74481bc53
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2024-10-14 14:23:25 +0500

    Debug

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import styles from "./Styles/page.module.css";
-import Image from "next/image";
 import Header from "@/app/Components/Header";
 import HeroPage from "./Components/Hero";
 import FooterPage from "./Components/Footer";
