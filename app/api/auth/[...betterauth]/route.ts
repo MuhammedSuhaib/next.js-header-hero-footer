729247c592a778cad0f190e4b8e9d15ad306ade4
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-12-19 17:35:35 +0500

    Cors

--- app/api/auth/[...betterauth]/route.ts
+++ app/api/auth/[...betterauth]/route.ts
@@ -1,11 +1,13 @@
 import { withCors } from "./../../../lib/auth";
 // export const runtime = "nodejs"; // IMPORTANT
 
-    export async function OPTIONS() {
+    export async function OPTIONS(request: Request) {
+    const origin = request.headers.get("origin");
+
     return new Response(null, {
         status: 204,
         headers: {
-        "Access-Control-Allow-Origin": "https://muhammedsuhaib.github.io",
+        "Access-Control-Allow-Origin": origin === "http://localhost:3000" ? origin : "https://muhammedsuhaib.github.io",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
         "Access-Control-Allow-Credentials": "true",
